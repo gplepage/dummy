49214ccad9294984fda2a1feaa07709fb59cb750
G. Peter Lepage <g.p.lepage@cornell.edu>
2023-10-07 16:54:06 -0400

    initial build

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy
+
+ext_args = dict(
+    include_dirs=[numpy.get_include()],
+    library_dirs=[],
+    runtime_library_dirs=[],
+    extra_link_args=[]
+    )
+
+ext_modules = [
+    Extension(name="dummy._code", sources=["src/dummpy/_code.pyx"], **ext_args),
+    ]
+
+setup(ext_modules=cythonize(ext_modules))
---
+++ src/dummy/_code.pyx
@@ -0,0 +1,7 @@
+import numpy as np 
+cimport numpy as np 
+np.import_array()
+
+def double(x):
+    return 2*x
+    
