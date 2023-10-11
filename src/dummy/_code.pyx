3feb62d57da32ccebf9aa266aa7057ec8588b290
G. Peter Lepage <g.p.lepage@cornell.edu>
2023-10-11 16:46:17 -0400

    wheels win

--- src/dummy/_code.pyx
+++ src/dummy/_code.pyx
@@ -1,8 +1,12 @@
 import numpy as np 
-cimport numpy as np 
-np.import_array()
+cimport numpy as cnp 
+cnp.import_array()
 
 def double(x):
+    cdef cnp.npy_intp i
+    a = np.array([0,1,2], dtype=np.intp)
+    for i in a:
+        a[i] += 1
     return 2*x
     
 def triple(x):
